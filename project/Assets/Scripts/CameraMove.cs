8bd7f81ac9bc4bd94467ce17978193d8a3fa3416
martinfontanet <martin.fontanet@epfl.ch>
2015-09-23 14:29:33 +0200

    setting the camera fixed

--- project/Assets/Scripts/CameraMove.cs
+++ project/Assets/Scripts/CameraMove.cs
@@ -14,15 +14,8 @@ public class CameraMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		float newAngle;
-		if(board.transform.eulerAngles.x > 180)
-			newAngle = (board.transform.eulerAngles.x-360);
-		else
-			newAngle = board.transform.eulerAngles.x;
-
-		newAngle = ( newAngle+ angle);
-		Vector3 offsetPos = new Vector3(0, Mathf.Sin ((newAngle)*Mathf.Deg2Rad)*radius, Mathf.Cos ((newAngle)*Mathf.Deg2Rad) * -radius);
+		Vector3 offsetPos = new Vector3(0, Mathf.Sin ((angle)*Mathf.Deg2Rad)*radius, Mathf.Cos ((angle)*Mathf.Deg2Rad) * -radius);
 		transform.position = player.transform.position + offsetPos;
-		transform.eulerAngles = new Vector3(newAngle,0,0);
+		transform.eulerAngles = new Vector3(angle,0,0);
 	}
 }
