ed6dfdcbe1349f7ebd1147236c2ac704e8ba62f6
martinfontanet <martin.fontanet@epfl.ch>
2015-09-29 12:15:15 +0200

    Scene 2

---
+++ project/Assets/Scripts/MovePickup.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePickup : MonoBehaviour {
+
+	public float speed;
+	
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3 (0, speed, 0) * Time.deltaTime);
+	}
+}
